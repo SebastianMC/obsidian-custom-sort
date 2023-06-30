e2712ea04d7bafe908c920cfc47fa9d06955e70e
SebastianMC <23032356+SebastianMC@users.noreply.github.com>
2023-06-30 19:01:16 +0200

    #79 - parent-folder-name variable support
    - unit tests for the testable part of updated custom-sort.ts

--- src/custom-sort/macros.spec.ts
+++ src/custom-sort/macros.spec.ts
@@ -44,7 +44,7 @@ function mockGroup(gprefix: string, group: string, prefix: string, full: string,
 }
 
 describe('expandMacros', () => {
-    it('should expand in all relevant text fields on all groups', () => {
+    it('should invoke expand in all relevant text fields on all groups', () => {
         const sortSpec: Partial<CustomSortSpec> = {
             groups: [
                 mockGroup('g-', '1-', 'abc', 'def', 'ghi'),
@@ -65,5 +65,35 @@ describe('expandMacros', () => {
         expect(sp).toHaveBeenNthCalledWith(4, 'gs-2-def', ParentFolder)
         expect(sp).toHaveBeenNthCalledWith(5, 'gs-2-abc', ParentFolder)
         expect(sp).toHaveBeenNthCalledWith(6, 'gs-2-ghi', ParentFolder)
+    })
+    it('should expand correctly in all relevant text fields on all groups, based on shadow groups', () => {
+        const sortSpec: Partial<CustomSortSpec> = {
+            groups: [
+                mockGroup('g-', '1-', 'abc{:%parent-folder-name%:}', 'de{:%parent-folder-name%:}f', '{:%parent-folder-name%:}ghi'),
+                mockGroup('g-', '2-', '{:%parent-folder-name%:}abc', 'd{:%parent-folder-name%:}ef', 'ghi{:%parent-folder-name%:}'),
+            ],
+            groupsShadow: [
+                mockGroup('gs-', '1-', 'abc{:%parent-folder-name%:}', 'de{:%parent-folder-name%:}f', '{:%parent-folder-name%:}ghi'),
+                mockGroup('gs-', '2-', '{:%parent-folder-name%:}abc', 'd{:%parent-folder-name%:}ef', 'ghi{:%parent-folder-name%:}'),
+            ]
+        }
+        const originalSortSpec: Partial<CustomSortSpec> = {
+            groups: [...sortSpec.groups!],
+            groupsShadow: [...sortSpec.groupsShadow!]
+        }
+        const ParentFolder = 'Parent folder name'
+        expandMacros(sortSpec as CustomSortSpec, ParentFolder)
+        expect(sortSpec.groups![0].exactText).toBe(originalSortSpec.groups![0].exactText)
+        expect(sortSpec.groups![0].exactPrefix).toBe(originalSortSpec.groups![0].exactPrefix)
+        expect(sortSpec.groups![0].exactSuffix).toBe(originalSortSpec.groups![0].exactSuffix)
+        expect(sortSpec.groups![1].exactText).toBe(originalSortSpec.groups![1].exactText)
+        expect(sortSpec.groups![1].exactPrefix).toBe(originalSortSpec.groups![1].exactPrefix)
+        expect(sortSpec.groups![1].exactSuffix).toBe(originalSortSpec.groups![1].exactSuffix)
+        expect(sortSpec.groupsShadow![0].exactText).toBe('gs-1-deParent folder namef')
+        expect(sortSpec.groupsShadow![0].exactPrefix).toBe('gs-1-abcParent folder name')
+        expect(sortSpec.groupsShadow![0].exactSuffix).toBe('gs-1-Parent folder nameghi')
+        expect(sortSpec.groupsShadow![1].exactText).toBe('gs-2-dParent folder nameef')
+        expect(sortSpec.groupsShadow![1].exactPrefix).toBe('gs-2-Parent folder nameabc')
+        expect(sortSpec.groupsShadow![1].exactSuffix).toBe('gs-2-ghiParent folder name')
     })
 })
